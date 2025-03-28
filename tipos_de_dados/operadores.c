f0e29d0d5e4c9d12a5702da301b6ecb6fa9163d9
HSA995 <henriquesouto995>
2025-03-28 11:53:14 -0300

    adição

--- tipos_de_dados/operadores.c
+++ tipos_de_dados/operadores.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv []){
 
     float n1, n2;
 
-    printf("Digite o valor de n1:");
-    scanf("%d", &n1);
+    printf("Digite o valor de n1: ");
+    scanf("%f", &n1);
 
-    printf("Digite o valor de n2:");
-    scanf("%d", &n2);
+    printf("Digite o valor de n2: ");
+    scanf("%f", &n2);
 
     // Adição
+    
+    float soma = n1 + n2;
+    printf("%f + %f = %f", n1, n2, soma);
 
     // Subtração
 
